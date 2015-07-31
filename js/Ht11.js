09c0c360f7f5c198459a07657b45da84c1c9f70a
Svetlana <sgorishna@gmail.com>
2015-07-31 17:25:08 +0300

    square color fixed

--- js/Ht11.js
+++ js/Ht11.js
@@ -1,35 +1,42 @@
-
 var interval;
 
 function showSquare() {
+
+	var div = document.getElementById('square');
 	
+	div.style.background = 'blue';
+	
+	div.style.left = '10px';
+
 	clearInterval(interval);
-	
-   document.getElementById('square').style.display = "block";
-   
-   interval = setInterval(function(){ move() }, 500);
-  
+
+	document.getElementById('square').style.display = "block";
+
+	interval = setInterval(function() {
+		move()
+	}, 500);
+
 }
 
-function move(){
-	var div = document.getElementById('square');	
-	
+function move() {
+	var div = document.getElementById('square');
+
 	div.style.left = parseFloat(getComputedStyle(div).left) + 42 + 'px';
-	
+
 }
 
+function stop() {
 
-function stop(){
-	
-	
 	clearInterval(interval);
 	
 	var div = document.getElementById('square');
 	
-	div.style.left = '10px';
-	
-	interval = setInterval(function(){ move() }, 500);
-	
-	
-	
+	div.style.background = 'red'; 
+
+	// var div = document.getElementById('square');
+	//	
+	// div.style.left = '10px';
+	//	
+	// interval = setInterval(function(){ move() }, 500);
+
 }
